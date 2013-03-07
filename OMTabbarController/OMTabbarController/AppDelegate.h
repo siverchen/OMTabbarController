03b765c6e7efae1cdd25b0ef8cdfbf5e030de8e9
siverchen <siverchen@gmail.com>
2013-03-07 17:06:34 +0800

    initialize OMTabbarcontroller

---
+++ OMTabbarController/OMTabbarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OMTabbarController
+//
+//  Created by Chen Lei on 13-3-7.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OMTabbarController/OMTabbarController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OMTabbarController
+//
+//  Created by Chen Lei on 13-3-7.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
